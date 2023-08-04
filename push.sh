affee22d461a8922962ae78281aac127959d0ca8
LiuJi0Fen04 <205844372@qq.com>
2023-08-04 21:31:54 +0800

    routine update

--- push.sh
+++ push.sh
@@ -4,5 +4,6 @@ folder=".git"
 if [ -d $folder ]; then
   echo "> find repository"
   echo "> pushing to origin..."
+  echo -e "push log" >> log.txt
   git add . && git commit  -m "routine update" && git push origin master
 fi
